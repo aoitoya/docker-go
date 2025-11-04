32d6b30f24c1ac40da7be2dd5d8d17452bd97bcf
toya <96932308+toyaAoi@users.noreply.github.com>
2025-11-05 05:00:00 +0530

    separate processes from host

--- main.go
+++ main.go
@@ -11,24 +11,48 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/vagrant/ubuntu-fs")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
 
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
+
+	syscall.Unmount("/proc", 0)
 }
